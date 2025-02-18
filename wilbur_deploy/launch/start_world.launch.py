bda01c46809f5b14b3b91eb3e3c5eb02b4d53aaf
Jodi Graf <jodi.s.graf@nasa.gov>
2025-02-18 15:54:12 -0600

    Fixed the start world launch file so that the world can be specified.  Defaults to rockkyard world

--- wilbur_deploy/launch/start_world.launch.py
+++ wilbur_deploy/launch/start_world.launch.py
@@ -2,7 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (DeclareLaunchArgument, SetEnvironmentVariable, 
-                            IncludeLaunchDescription, SetLaunchConfiguration)
+                            IncludeLaunchDescription, OpaqueFunction, SetLaunchConfiguration)
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, TextSubstitution
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -10,35 +10,52 @@ from launch.substitutions import FindExecutable
 from launch.actions import ExecuteProcess
 
 
+arguments = []
 
-def generate_launch_description():
+arguments.append(DeclareLaunchArgument(
+    'world',
+    default_value='rockyard.sdf',
+    description='name of the world file',
+))
+
+
+arguments.append(DeclareLaunchArgument(
+    'world_pkg',
+    default_value='rockyard_world',
+    description='name of the package that has the world file',
+))
+
+
+def launch_setup(context):
+    
+    # Initialize Arguments
+    world = LaunchConfiguration("world").perform(context)
+    world_pkg = LaunchConfiguration("world_pkg").perform(context)
+    
+    pkg_deploy = get_package_share_directory('wilbur_deploy')
+    pkg_description = get_package_share_directory('wilbur_description')
     
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    pkg_deploy = get_package_share_directory('phoebe_deploy')
-    pkg_description = get_package_share_directory('phoebe_description')
     gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
     gz_model_path = PathJoinSubstitution([pkg_description, 'urdf'])
     
-    
-    
-    world = str(os.path.join(pkg_deploy, 'worlds', 'empty_world.sdf'))
-    
-    arguments = []
-    arguments.append(DeclareLaunchArgument(
-            'world',
-            default_value='empty_world',
-            description='World to load into Gazebo'
-        ))
+    world_file = str(os.path.join(get_package_share_directory(world_pkg), 'worlds', world))
+    if(not world_file.endswith('.sdf')):
+        world_file += '.sdf'
+        
+    if(not os.path.isfile(world_file)):
+        print('********************************************')
+        print('world file (', world_file, ') does not exist, using default empty world')
+        print('********************************************')
+        world_file = str(os.path.join(pkg_deploy, 'worlds', 'empty_world.sdf'))
     
     actions = []
-    actions.append(SetLaunchConfiguration(name='world_file', 
-                               value=[LaunchConfiguration('world'), 
-                                      TextSubstitution(text='.sdf')]))
+    
     actions.append(SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_model_path))
     actions.append(IncludeLaunchDescription(
             PythonLaunchDescriptionSource(gz_launch_path),
             launch_arguments={
-                'gz_args': ['-r -v 4 ' + world], # -r to unpause the sim (required to load controls) -v verbose
+                'gz_args': ['-r -v 4 ' + world_file], # -r to unpause the sim (required to load controls) -v verbose
                 'on_exit_shutdown': 'True'
             }.items(),
         ))
@@ -53,4 +70,11 @@ def generate_launch_description():
     nodes = []
     nodes.append(clock_bridge)
     
-    return(LaunchDescription(arguments + actions + nodes))
+    return (nodes + actions)
+    
+    
+def generate_launch_description():
+    n = OpaqueFunction(function=launch_setup)
+    ld = LaunchDescription(arguments)
+    ld.add_action(n)
+    return ld
