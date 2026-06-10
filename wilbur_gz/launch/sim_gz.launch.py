fb63243e6172bc0dbc5f72bcb4f24f5764e04b5c
Misha Savchenko <misha.savchenko@nasa.gov>
2026-06-10 08:03:12 -0500

    passing include_ur launch argument from the gazebo launch

--- wilbur_gz/launch/sim_gz.launch.py
+++ wilbur_gz/launch/sim_gz.launch.py
@@ -4,7 +4,12 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, OpaqueFunction
+from launch.actions import (
+    IncludeLaunchDescription,
+    DeclareLaunchArgument,
+    GroupAction,
+    OpaqueFunction,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, PushRosNamespace
@@ -20,6 +25,8 @@ def launch_setup(context, *args, **kwargs):
     y = LaunchConfiguration("robot_y")
     z = LaunchConfiguration("robot_z")
 
+    include_ur = LaunchConfiguration("include_ur")
+
     if namespace.perform(context) == "":
         use_namespace = "False"
     else:
@@ -34,7 +41,9 @@ def launch_setup(context, *args, **kwargs):
     world_group = GroupAction(
         [
             IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(os.path.join(pkg_gazebo, "launch", "start_world.launch.py"))
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_gazebo, "launch", "start_world.launch.py")
+                )
             )
         ]
     )
@@ -43,7 +52,9 @@ def launch_setup(context, *args, **kwargs):
     # add the robot to the world
     robot_group = GroupAction(
         [
-            PushRosNamespace(condition=IfCondition([use_namespace]), namespace=namespace),
+            PushRosNamespace(
+                condition=IfCondition([use_namespace]), namespace=namespace
+            ),
             Node(
                 package="ros_gz_sim",
                 executable="create",
@@ -71,19 +82,24 @@ def launch_setup(context, *args, **kwargs):
                 name="sim_bridge",
                 parameters=[
                     {
-                        "config_file": os.path.join(pkg_gazebo, "config", "bridge.yaml"),
+                        "config_file": os.path.join(
+                            pkg_gazebo, "config", "bridge.yaml"
+                        ),
                         "qos_overrides./tf_static.publisher.durability": "transient_local",
                     }
                 ],
                 output="screen",
             ),
             IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(os.path.join(pkg_deploy, "launch", "control.launch.py")),
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_deploy, "launch", "control.launch.py")
+                ),
                 launch_arguments={
                     "platform": "sim_ignition",
                     "separate_controls_pcs": "false",
                     "tf_prefix": tf_prefix,
                     "ns": namespace,
+                    "include_ur": include_ur,
                 }.items(),
             ),
         ]
@@ -135,5 +151,15 @@ def generate_launch_description():
             description="namespace of the robot",
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "include_ur",
+            default_value="true",
+            description="Adds/removes the UR10e arm from the configuration.",
+            choices=["true", "false"],
+        )
+    )
 
-    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
+    return LaunchDescription(
+        declared_arguments + [OpaqueFunction(function=launch_setup)]
+    )
