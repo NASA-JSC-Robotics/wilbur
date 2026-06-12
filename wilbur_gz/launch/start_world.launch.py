fa10c473b0383a09af1ec47d880e1e78f408bc76
Erik Holum <erik.holum@nasa.gov>
2026-06-12 13:50:34 -0500

    Empty world launch for wilbur

--- wilbur_gz/launch/start_world.launch.py
+++ wilbur_gz/launch/start_world.launch.py
@@ -18,93 +18,54 @@
 # under the License.
 
 
-import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    IncludeLaunchDescription,
-    OpaqueFunction,
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
 )
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
-arguments = []
+def generate_launch_description():
 
-arguments.append(
-    DeclareLaunchArgument(
+    world_arg = DeclareLaunchArgument(
         "world",
-        default_value="rockyard.sdf",
+        default_value="empty_world.sdf",
         description="name of the world file",
     )
-)
 
-
-arguments.append(
-    DeclareLaunchArgument(
+    world_pkg_arg = DeclareLaunchArgument(
         "world_pkg",
-        default_value="jsc_rockyard_world_gz",
+        default_value="wilbur_gz",
         description="name of the package that has the world file",
     )
-)
 
-
-def launch_setup(context):
-
-    # Initialize Arguments
-    world = LaunchConfiguration("world").perform(context)
-    world_pkg = LaunchConfiguration("world_pkg").perform(context)
-
-    pkg_deploy = get_package_share_directory("wilbur_deploy")
+    world_file = PathJoinSubstitution(
+        [FindPackageShare(LaunchConfiguration("world_pkg")), "worlds", LaunchConfiguration("world")]
+    )
 
     pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
     gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, "launch", "gz_sim.launch.py"])
 
-    world_file = str(os.path.join(get_package_share_directory(world_pkg), "worlds", world))
-    if not world_file.endswith(".sdf"):
-        world_file += ".sdf"
-
-    if not os.path.isfile(world_file):
-        print("********************************************")
-        print("world file (", world_file, ") does not exist, using default empty world")
-        print("********************************************")
-        world_file = str(os.path.join(pkg_deploy, "worlds", "empty_world.sdf"))
-
-    actions = []
-
-    # TODO: option for gazebo headless
-    # actions.append(SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_model_path))
-    # wrong resource path - should be IGN_GAZEBO_RESOURCE_PATH for fortress and ignition
-    # https://gazebosim.org/docs/fortress/ros_gz_project_template_guide/
-    actions.append(
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gz_launch_path),
-            launch_arguments={
-                "gz_args": ["-r -v 4 " + world_file],  # -r to unpause the sim (required to load controls) -v verbose
-                "on_exit_shutdown": "True",
-            }.items(),
-        )
+    # TODO: Add an option for gazebo headless?
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gz_launch_path),
+        launch_arguments={
+            "gz_args": ["-r -v 4 ", world_file],
+            "on_exit_shutdown": "True",
+        }.items(),
     )
 
     clock_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
         name="clock_bridge",
-        arguments=[
-            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
-        ],
+        arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
         output="screen",
     )
-    nodes = []
-    nodes.append(clock_bridge)
 
-    return nodes + actions
-
-
-def generate_launch_description():
-    n = OpaqueFunction(function=launch_setup)
-    ld = LaunchDescription(arguments)
-    ld.add_action(n)
-    return ld
+    return LaunchDescription([world_arg, world_pkg_arg, gz_sim, clock_bridge])
