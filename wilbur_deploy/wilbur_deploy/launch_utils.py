3acbc7545bc16376f7aed39ff14aec3df11e007d
Jodi Graf <jodi.s.graf@nasa.gov>
2026-07-28 14:14:05 -0500

    Switched to the phoebe format for the spawn_controller function

--- wilbur_deploy/wilbur_deploy/launch_utils.py
+++ wilbur_deploy/wilbur_deploy/launch_utils.py
@@ -6,7 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
-
+from launch.substitutions import LaunchConfiguration
 
 # helper function to organize launch description objects with the same launch args and package names
 def AddLaunchDescriptions(package_name, launch_file_names, launch_args):
@@ -40,21 +40,35 @@ def GetControllersFile(package_name, file_name):
 
 
 # Helper function to make controller nodes
-def SpawnController(
-    controller_manager_name: str, controller_name: str, controller_manager_timeout: str = "300", **kwargs
-) -> Node:
+def spawn_controller(
+    controller_name,
+    inactive=False,
+    controller_manager_name="controller_manager",
+    timeout=300,
+    namespace: LaunchConfiguration = "",
+    condition=None,
+    controller_ros_args=None,
+):
+    """
+    Create a spawn controller node action for the specified controller and arguments.
+    """
+    inactive_flags = ["--inactive"] if inactive else []
+    ros_args = ["--controller-ros-args", f"{controller_ros_args}"] if controller_ros_args is not None else []
+
     return Node(
         package="controller_manager",
         executable="spawner",
         name=controller_name,
+        namespace=namespace,
         arguments=[
+            controller_name,
             "--controller-manager",
             controller_manager_name,
             "--controller-manager-timeout",
-            controller_manager_timeout,
-            "--controller",
-            controller_name,
-        ],
-        output="screen",
-        **kwargs
+            str(timeout),
+        ]
+        + ros_args
+        + inactive_flags,
+        output="both",
+        condition=condition,
     )
