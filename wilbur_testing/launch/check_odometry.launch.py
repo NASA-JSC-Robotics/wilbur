c1639c9e78556447edf7cfbc49a1128d98987eaf
Jodi Graf <jodi.s.graf@nasa.gov>
2026-08-19 14:15:28 -0500

    check_odometry launch time based

--- wilbur_testing/launch/check_odometry.launch.py
+++ wilbur_testing/launch/check_odometry.launch.py
@@ -2,7 +2,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, TimerAction, RegisterEventHandler, DeclareLaunchArgument
 from launch.event_handlers import OnProcessStart, OnProcessExit
-from launch.actions import RegisterEventHandler, EmitEvent
+from launch.actions import RegisterEventHandler, EmitEvent, LogInfo
 from launch_ros.actions import Node
 from launch.events import Shutdown
 from launch.substitutions import LaunchConfiguration, PythonExpression
@@ -27,22 +27,22 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "count",
-            default_value="100",
-            description="Number of commands to send",
+            "time_ms",
+            default_value="1000",
+            description="Length of time to send commands (sec)",
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
             'separate',
-            default_value='true',
+            default_value='false',
             description="log topics in separate files",
         )
     )
 
     x_dot = LaunchConfiguration("x_dot")
     theta_dot = LaunchConfiguration("theta_dot")
-    count = LaunchConfiguration("count")
+    time = LaunchConfiguration("time_ms")
     sep = LaunchConfiguration('separate')
 
     log_odom = Node(
@@ -59,9 +59,20 @@ def generate_launch_description():
         output="screen",
         parameters=[{'x_dot': x_dot},
                     {'theta_dot': theta_dot},
-                    {'count': count}],
+                    {'time': time}],
+    )
+
+    # Create the event handler to shutdown everything when critical_node exits
+    shutdown_handler = RegisterEventHandler(
+        OnProcessExit(
+            target_action=send_cmd,
+            on_exit=[
+                LogInfo(msg=['Critical node died! Shutting down launch description...']),
+                EmitEvent(event=Shutdown())
+            ]
+        )
     )
 
     return LaunchDescription(declared_arguments + [
-        log_odom, send_cmd
+        log_odom, send_cmd, shutdown_handler
     ])
